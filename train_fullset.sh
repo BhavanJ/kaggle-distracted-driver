962c76b88c3d7320191fd583391eec463e939b79
HEGD0001 <HEGD0001@e.ntu.edu.sg>
2016-06-30 01:46:08 +0800

    report gen

--- train_fullset.sh
+++ train_fullset.sh
@@ -2,4 +2,5 @@
 python pickle_processor.py --train ../test/rcnn/output/kaggle_full_trainset_bbox_objset_1.pkl \
 	../test/rcnn/output/kaggle_full_trainset_bbox_objset_4.pkl \
 	--csv ../driver_imgs_list.csv \
-	--test ../test/rcnn/test-box/teset_set_batch_0_bbox.pkl ../test/rcnn/test-box/teset_set_batch_1_bbox.pkl
+	--test ../test/rcnn/test-box/teset_set_batch_0_bbox.pkl ../test/rcnn/test-box/teset_set_batch_1_bbox.pkl \
+		../test/rcnn/test-box/teset_set_batch_2_bbox.pkl ../test/rcnn/test-box/teset_set_batch_3_bbox.pkl
