f543757054bab96992fdcc467a08592633390e85
HEGD0001 <HEGD0001@e.ntu.edu.sg>
2016-07-07 01:20:39 +0800

    head based classifier

--- head_based_classifier.py
+++ head_based_classifier.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
 import csv, cPickle
+import math
 
 CLASSES = ('c0', 'c1', 'c2', 'c3','c4','c5','c6','c7','c8','c9')
 
@@ -11,7 +12,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Takes detection files to generate features.')
     parser.add_argument('--train', dest='train_pkl_files', nargs='+', help='Training object pickle files')
     parser.add_argument('--csv', dest='csv', default=None, help='Class info csv file')
-    #parser.add_argument('--test', dest='test_pkl_files', nargs='+', help='Training object pickle files')
+    parser.add_argument('--test', dest='test_pkl_files', nargs='+', help='Training object pickle files')
     args = parser.parse_args()
     return args
 
@@ -36,6 +37,39 @@ def get_likely_class(img_boxes):
         rand_guess = True
     return pred_cat, rand_guess
 
+def get_class_prob(objs):
+    cat_score = [0.0]*10
+    # get top score of all detected boxes per category.
+    found = False
+    for obj, boxes in objs.iteritems():
+        cat = obj.split('_')[0]
+        if(len(boxes) == 1):
+            cat_score[CLASSES.index(cat)] = boxes[0][4]
+            found = True
+        elif(len(boxes) > 1):
+            score = [b[4] for b in boxes]
+            cat_score[CLASSES.index(cat)] = max(score)
+            found = True
+
+    if(found):
+        pred_cat_idx = cat_score.index(max(cat_score))
+        residue = 1.0 - max(cat_score)
+
+        other_prob = np.random.rand(9)
+        other_prob = other_prob/other_prob.sum()
+        other_prob = other_prob * residue
+
+        other = 0
+        prob = [0.0]*10
+        prob[pred_cat_idx] = max(cat_score)
+        for c in range(10):
+            if(c != pred_cat_idx):
+                prob[c] = other_prob[other]
+                other += 1
+    else:
+        prob = [0.1]*10
+    return prob
+    
 def show_confusion_matrix(mat):
     mat = mat.astype(np.float32)
     cls_total = np.sum(mat, axis=1)
@@ -74,6 +108,7 @@ def compute_accuracy(det_file, csv_label_file):
     report = np.zeros(shape=(len(CLASSES), len(CLASSES)), dtype=np.int32)
     total_err_cnt = 0
     rand_guess_cnt = 0
+    loss = 0.0
     for img, act_cls in cls_info.iteritems():
         img_boxes = pred_boxes[img]
         pred_cls, rand = get_likely_class(img_boxes)
@@ -87,12 +122,36 @@ def compute_accuracy(det_file, csv_label_file):
 
         report[act_idx, pred_idx] = report[act_idx, pred_idx] + 1
 
+        prob = get_class_prob(img_boxes)
+        p_i = prob[act_idx]
+        p_i = max(min(p_i, 1-1e-15), 1e-15)
+        loss += math.log(p_i)
+
+    loss = -loss/len(cls_info)
+        
+
     print('Overall error  = {:f}%'.format(float(total_err_cnt)/len(cls_info)))
     print('Total random guesses = {:d}'.format(rand_guess_cnt))
+    print('Overall loss = {:f}'.format(loss))
     show_confusion_matrix(report)
+
+def generate_test_predictions(pkl_files):
+
+    obj_dict = {}
+    for pkl_file in pkl_files:
+        with open(pkl_file, 'r') as pf:
+            obj_dict.update(cPickle.load(pf)['boxes'])
+
+    prob_dict = {}
+    for img, objs in obj_dict.iteritems():
+        prob_dict[img] = get_class_prob(objs)
+
+    with open('head_based_test_predictions.pkl', 'w') as tf:
+        cPickle.dump(prob_dict, tf)
 
 if __name__=='__main__':
     args = parse_args()
     compute_accuracy(args.train_pkl_files[0], args.csv)
+    generate_test_predictions(args.test_pkl_files)
 
 
---
+++ head_pred.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dir=$(dirname "$0")
+echo $dir
+test_bbox_dir="$dir/../test/rcnn/test-box-head"
+echo $test_bbox_dir
+test_bbox_files="$test_bbox_dir/*.pkl"
+echo $test_bbox_files
+python head_based_classifier.py --train ../test/rcnn/output/kaggle_full_trainset_bbox_objset_head.pkl \
+	--csv ../driver_imgs_list.csv \
+	--test $test_bbox_files
+echo "Generating submission file..."
+python -u  result_gen.py --pred head_based_test_predictions.pkl --list ../test_set.txt
